4ce581693c3d74ffbcc66fb43318c5e7e1b5d204
FrancoBulotas <francothiago10@hotmail.com>
2024-05-28 21:44:12 -0300

    up to 3.18

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -47,11 +47,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  // const id = Number(request.params.id)
-  // const person = persons.find(person => person.id === id)
-  
-  
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
   .then(person => {  
     if (person) {
@@ -61,37 +57,25 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()  
     }
   })
-  .catch(error => {
-    console.log(error)
-    response.status(500).end()    })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
     response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${getCurrentDate()}</p>
   `)
   })
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-
-    Person.findById(id).then(person => {
-      console.log(person)
-      response.json(person)
-    })
-    // persons = persons.filter(person => person.id !== id)
-  
-    // response.status(204).end()
 })
 
-const generateId = () => {
-  const randomValue = Math.random()
-  if (randomValue !== 0){
-    return Math.floor(randomValue * 10000)
-  }
-}
-  
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
@@ -120,6 +104,43 @@ app.post('/api/persons', (request, response) => {
       response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new : true})
+  .then(updatedPerson => {
+    console.log(updatedPerson)
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
